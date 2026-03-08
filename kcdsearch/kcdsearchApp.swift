bd8ff4c2190d11d1af9aaa1367a2d75fd1c7f64f
Paras Khanchandani <dev@paraskcd.com>
2026-03-08 20:40:30 +0100

    Initial Commit

---
+++ kcdsearch/kcdsearchApp.swift
@@ -0,0 +1,17 @@
+//
+//  kcdsearchApp.swift
+//  kcdsearch
+//
+//  Created by ParasKCD on 8/3/26.
+//
+
+import SwiftUI
+
+@main
+struct kcdsearchApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
